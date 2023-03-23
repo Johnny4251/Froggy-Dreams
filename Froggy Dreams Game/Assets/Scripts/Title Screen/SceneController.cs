b68d356de6b15bc0203ef80ce677dfaec3f3a834
John Pertell <johnangus425@gmail.com>
2023-03-23 16:05:41 -0400

    Background revamp

---
+++ Froggy Dreams Game/Assets/Scripts/Title Screen/SceneController.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneController : MonoBehaviour
+{
+
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            Debug.Log("Clicked");
+        }    
+    }
+}
