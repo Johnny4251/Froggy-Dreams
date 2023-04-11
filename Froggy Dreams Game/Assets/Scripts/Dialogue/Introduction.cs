69063f107d44bc67999a36072b13ed28783c49a4
Laddie Mawery <laddiemawery@yahoo.com>
2023-04-11 12:22:40 -0400

    null

---
+++ Froggy Dreams Game/Assets/Scripts/Dialogue/Introduction.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Introduction : MonoBehaviour
+{
+
+    private Dialogue dialogue;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+}
